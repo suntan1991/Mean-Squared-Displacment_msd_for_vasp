6111e9d09d70b92e1b70f9790b846a5532bda9d0
suntan1991 <31029512+suntan1991@users.noreply.github.com>
2018-04-29 21:33:37 +0800

    Update msd.py

--- msd.py
+++ msd.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 28 00:24:14 2018
+Created on Sat Apr 28 00:24:14 2018, version 3
 
 @author: jliubt
+Contact: suntan1991@gmail.com
 """
 
-# This script is based on the previous one developed by Muratahan Aykol
+# This script is inpsired by previous one developed by Muratahan Aykol
 # To accurately calculate the msd with block sampling
 # Every neighbouring time steps should check with nearest images considering pbc
 # It is better to use this code for total time steps larger than 10,000. This is
-# very common in any MD simulations, both classical and ab-initio
+# very common in any MD simulations, both classical and ab-initio MD
 import numpy as np
-import os, pickle
+import os, pickle, time
 
 
 def MSD(xdata_file):
@@ -73,17 +74,12 @@ def MSD(xdata_file):
     
     ###########################################################################
     # going to calculate the Mean-Squared-Displacement
-    # get the distance btw two cascade time steps, and then add N_block distances together to get msd
-    diff_pos = np.diff(pos, axis=0)                                             # distance in fractional coordinate
-    #get nearest distance consiering pbc, e.g., 0.2-->0.2; 0.8-->-0.2; -0.3-->-0.3; -0.7-->0.3
-    diff_pos = diff_pos - np.rint(diff_pos)
     # minimum unit steps for msd sampling, this can be adjusted if needed
     block = 1
     N_block = (N_frame-1) // block
     msd_block = np.zeros((N_block, N_atoms))
     msd_lattice_block = np.zeros((N_block, N_atoms,3))
     
-    N_jump = 100                                                                # steps jumped for sampling
     for k, step in enumerate(range(block, N_frame, block)):
         msd_sample = np.zeros((N_atoms))
         msd_lattice_sample = np.zeros((N_atoms,3))
@@ -91,26 +87,21 @@ def MSD(xdata_file):
         # divide the total time steps into small blocks, one block with [step] images
         # and sum all the images in subblock, then average
         # Here I use two differnt schemes for sampling: 
-        if step <= N_frame/100:
-            Frame_ava = (N_frame-1) - np.mod(N_frame-1, step)                       # Frames available
-            diff_pos_copy = []                                                      # Erase the copy of position difference
-            diff_pos_copy = np.reshape(diff_pos[:Frame_ava], (-1,step,N_atoms,3))   # -1 is dimension left to be compatible
-            dist = np.sum(diff_pos_copy, axis=1)
-            dist = np.dot(dist,lattice)                                             # distance in cartesian coordinate
+        # get the distance btw two cascade time steps, and then add N_block distances together to get msd
+        count = 0
+        for j in range(0, step, 10):
+            pos_copy = []
+            pos_copy = pos[j::step]                                             # Sampling for every [step]th entry 
+            diff_pos = np.diff(pos_copy, axis=0)                                # distance in fractional coordinate
+            #get nearest distance consiering pbc, e.g., 0.2-->0.2; 0.8-->-0.2; -0.3-->-0.3; -0.7-->0.3
+            dist = diff_pos - np.rint(diff_pos)
+            dist = np.dot(dist,lattice)                                         # distance in cartesian coordinate
             dist_square = dist*dist
-            msd_lattice_sample = np.mean(dist_square,axis=0)                        # take average of different sampling
-            msd_sample = np.sum(msd_lattice_sample, axis=1)
-        else:
-            count = 0
-            for j in range(0, N_frame-step, N_jump):
-                dist = np.sum(diff_pos[j:j+step], axis=0)
-                dist = np.dot(dist,lattice)
-                dist_square = dist*dist
-                msd_lattice_sample += dist_square
-                msd_sample += np.sum(dist_square, axis=1)
-                count += 1
-            msd_lattice_sample /= count
-            msd_sample /= count
+            msd_lattice_sample += np.sum(dist_square,axis=0)                    # take average of different sampling
+            msd_sample += np.sum(np.sum(dist_square,axis=0), axis=1)
+            count += len(diff_pos)
+        msd_lattice_sample /= count
+        msd_sample /= count
 
         if step % 100 == 0:
             print (step)
@@ -128,5 +119,8 @@ def MSD(xdata_file):
             el_index_start = el_index_end
         recorder.write("\n")
     recorder.close()
-    
+
+start_time = time.clock()
 MSD("XDATCAR")
+time_cost = time.clock() - start_time
+print("------ Time used: %s ------" % time.strftime("%H:%M:%S", time.gmtime(time_cost)))
